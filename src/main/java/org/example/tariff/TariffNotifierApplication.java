dd38c9a2e7577f8259884e32a7e917f47474ba54
root <root@homer>
2017-11-13 02:20:33 +0700

    new commit

--- src/main/java/org/example/tariff/TariffNotifierApplication.java
+++ src/main/java/org/example/tariff/TariffNotifierApplication.java
@@ -1,12 +1,19 @@
 package org.example.tariff;
 
-import org.springframework.boot.SpringApplication;
+import org.springframework.boot.Banner.Mode;
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class TariffNotifierApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TariffNotifierApplication.class, args);
+		new SpringApplicationBuilder()
+                        .sources(TariffNotifierApplication.class)
+                        .bannerMode(Mode.OFF)
+                        .run(args);
+                
+                
 	}
 }
