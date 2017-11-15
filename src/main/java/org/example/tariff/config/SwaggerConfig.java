e5aeb5ae904987bad7742dfe7ce038b9595f3c81
root <root@homer>
2017-11-15 10:41:14 +0700

    new commit

--- src/main/java/org/example/tariff/config/SwaggerConfig.java
+++ src/main/java/org/example/tariff/config/SwaggerConfig.java
@@ -3,6 +3,7 @@ package org.example.tariff.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
 import static springfox.documentation.builders.PathSelectors.regex;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
@@ -20,7 +21,7 @@ public class SwaggerConfig {
 	    return new Docket(DocumentationType.SWAGGER_2)
                     .select()                 
                     .apis(RequestHandlerSelectors.basePackage("org.example.tariff.web.controllers"))
-	            .paths(regex("/api-tariff-notifier.*"))
+	            .paths(PathSelectors.any())
 	            .build();
 
 	  }
